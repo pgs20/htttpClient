a705e9cad3e25c72b54551e564cd82592f0a6e90
Gosha <gosha_petrov20@rambler.ru>
2024-02-06 21:34:38 +0300

    Создал простейшего http-клиента

---
+++ src/main/java/Cat.java
@@ -0,0 +1,37 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Cat {
+    private String id;
+    private String text;
+    private String type;
+    private String user;
+    private String upvotes;
+
+    public Cat(
+            @JsonProperty("id") String id,
+            @JsonProperty("text") String text,
+            @JsonProperty("type") String type,
+            @JsonProperty("user") String user,
+            @JsonProperty("upvotes") String upvotes
+    ) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    public String getUpvotes() {
+        return upvotes;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat" +
+                "\n id = " + id +
+                "\n text = " + text +
+                "\n type = " + type +
+                "\n user = " + user +
+                "\n upvotes = " + upvotes;
+    }
+}
